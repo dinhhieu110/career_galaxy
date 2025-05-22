89a49a978a3e2ef37ddcae92ce44efac13a8829b
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-05-22 15:53:44 +0700

    [CG-03] Implement login function by Gmail, FB & Linkedin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,6 +11,12 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { LinkIcon, LogOut, UserIcon } from "lucide-react";
 import { BarLoader } from "react-spinners";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 const Header = () => {
   const navigate = useNavigate();
 
@@ -34,52 +40,12 @@ const Header = () => {
           Career Galaxy
         </Link>
         <div>
-          {user ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger className="w-8 rounded-full overflow-hidden">
-                <Avatar>
-                  <AvatarImage
-                    src={user?.user_metadata?.profile_pic}
-                    className="object-cover"
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>
-                  {user?.user_metadata?.name}
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <UserIcon className="h-4 w-4" />
-                  Profile
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Link to="/dashboard" className="flex items-center gap-2">
-                    <LinkIcon className="h-4 w-4" />
-                    My URLs
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-red-400">
-                  <LogOut className="h-4 w-4" />
-                  <span
-                    onClick={() => {
-                      navigate("/");
-                    }}
-                  >
-                    Sign out
-                  </span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          ) : (
-            <Button
-              className="h-full bg-[#0186da] hover:bg-[#0186da] cursor-pointer"
-              onClick={() => navigate("/auth")}
-            >
-              Login
-            </Button>
-          )}
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
       </nav>
       {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
