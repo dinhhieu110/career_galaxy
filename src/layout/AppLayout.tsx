e0f77a65b725432df1c035ea9d1543ed108d3906
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-05-22 15:07:13 +0700

    [CG-01] Build responsive footer component

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,14 +1,16 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
 
 const AppLayout = () => {
   return (
     <Fragment>
-      <main>
-        {/* Header */}
+      <main className="min-h-screen px-4 sm:px-8 lg:px-16 xl:px-22 2xl:px-24">
+        <Header />
         <Outlet />
       </main>
-      {/* Footer */}
+      <Footer />
     </Fragment>
   );
 };
