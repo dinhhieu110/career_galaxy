b80a461fcf982e82ee75d88779a724172c4f853e
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-05-23 11:48:39 +0700

    [CG-11] Build responsive login component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,3 +5,4 @@ export { default as Job } from "./Job";
 export { default as MyJobs } from "./MyJobs";
 export { default as PostJob } from "./PostJob";
 export { default as SavedJobs } from "./SavedJobs";
+export { default as Auth } from "./Auth";
