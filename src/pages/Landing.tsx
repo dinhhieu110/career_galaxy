959ba0a197a005cc492e066fa1eafa1abcbb8d30
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-05-23 10:24:48 +0700

    [CG-08] Build responsive landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,41 @@
+import CompaniesSlider from "@/components/CompaniesSlider";
+import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItWorks";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+      <Hero />
+      <CompaniesSlider />
+      <img src="/banner.jpeg" alt="banner image" className="w-full" />
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>For Job Seekers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Search and apply for jobs, track application, and more.</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>For Employers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Post job, manage applications, and find the best candidate
+          </CardContent>
+        </Card>
+      </section>
+      <HowItWorks />
+    </main>
+  );
 };
 
 export default Landing;
